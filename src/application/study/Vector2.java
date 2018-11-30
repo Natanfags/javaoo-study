67e67914244511a5eee68c8095b6fa2d4661b85d
Natan Fagundes <natan.fags@gmail.com>
2018-11-30 11:05:57 -0200

    {Curse-OO} Utilizado atributo .length

--- src/application/study/Vector2.java
+++ src/application/study/Vector2.java
@@ -14,7 +14,7 @@ public class Vector2 {
 		int n = sc.nextInt();
 		Product[] vect = new Product[n];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < vect.length; i++) {
 			sc.nextLine();
 			String name = sc.nextLine();
 			double price = sc.nextDouble();
@@ -22,7 +22,7 @@ public class Vector2 {
 		}
 
 		double sum = 0.0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < vect.length; i++) {
 			sum += vect[i].getPrice();
 		}
 
